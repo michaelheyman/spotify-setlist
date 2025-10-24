7be8e8139f1264e5cc43045ae0ec7fc57453bf3a
Michael Heyman <michaelheyman@users.noreply.github.com>
2025-10-24 16:54:06 -0700

    Make Setlist FM service private

--- internal/interfaces/setlist_fm_service.go
+++ internal/interfaces/setlist_fm_service.go
@@ -14,90 +14,21 @@ const (
 	baseURL      = "https://api.setlist.fm/rest/1.0"
 )
 
-type searchArtistResponse struct {
-	Artist []struct {
-		Mbid           string `json:"mbid"`
-		Name           string `json:"name"`
-		SortName       string `json:"sortName"`
-		Disambiguation string `json:"disambiguation"`
-		URL            string `json:"url"`
-	} `json:"artist"`
-	Total        int `json:"total"`
-	Page         int `json:"page"`
-	ItemsPerPage int `json:"itemsPerPage"`
-}
-
-type getArtistSetlistsResponse struct {
-	Type         string `json:"type"`
-	ItemsPerPage int    `json:"itemsPerPage"`
-	Page         int    `json:"page"`
-	Total        int    `json:"total"`
-	Setlist      []struct {
-		ID          string `json:"id"`
-		VersionID   string `json:"versionId"`
-		EventDate   string `json:"eventDate"`
-		LastUpdated string `json:"lastUpdated"`
-		Artist      struct {
-			Mbid           string `json:"mbid"`
-			Name           string `json:"name"`
-			SortName       string `json:"sortName"`
-			Disambiguation string `json:"disambiguation"`
-			URL            string `json:"url"`
-		} `json:"artist"`
-		Venue struct {
-			ID   string `json:"id"`
-			Name string `json:"name"`
-			City struct {
-				ID        string `json:"id"`
-				Name      string `json:"name"`
-				State     string `json:"state"`
-				StateCode string `json:"stateCode"`
-				Coords    struct {
-					Lat  float64 `json:"lat"`
-					Long float64 `json:"long"`
-				} `json:"coords"`
-				Country struct {
-					Code string `json:"code"`
-					Name string `json:"name"`
-				} `json:"country"`
-			} `json:"city"`
-			URL string `json:"url"`
-		} `json:"venue"`
-		Sets struct {
-			Set []struct {
-				Song []struct {
-					Name string `json:"name"`
-					Info string `json:"info,omitempty"`
-					With struct {
-						Mbid           string `json:"mbid"`
-						Name           string `json:"name"`
-						SortName       string `json:"sortName"`
-						Disambiguation string `json:"disambiguation"`
-						URL            string `json:"url"`
-					} `json:"with,omitempty"`
-				} `json:"song"`
-			} `json:"set"`
-		} `json:"sets"`
-		URL  string `json:"url"`
-		Info string `json:"info,omitempty"`
-	} `json:"setlist"`
-}
-
-type SetlistFMService struct {
+type setlistFMService struct {
 	client  *http.Client
 	apiKey  string
 	baseURL string
 }
 
-func NewSetlistFMService(client *http.Client, apiKey string) *SetlistFMService {
-	return &SetlistFMService{
+func NewSetlistFMService(client *http.Client, apiKey string) *setlistFMService {
+	return &setlistFMService{
 		client:  client,
 		apiKey:  apiKey,
 		baseURL: baseURL,
 	}
 }
 
-func (s SetlistFMService) GetSetlists(ctx context.Context, artist string, opts ...domain.GetSetlistOption) ([]domain.Setlist, error) {
+func (s setlistFMService) GetSetlists(ctx context.Context, artist string, opts ...domain.GetSetlistOption) ([]domain.Setlist, error) {
 	// Get the artist mbid (Musicbrainz MBID) from the Setlist.fm API
 	mbid, err := s.getArtistMBID(ctx, artist)
 	if err != nil {
@@ -114,7 +45,7 @@ func (s SetlistFMService) GetSetlists(ctx context.Context, artist string, opts .
 }
 
 // getArtistMBID retrieves the Musicbrainz MBID for the given artist from Setlist.fm API
-func (s SetlistFMService) getArtistMBID(ctx context.Context, artist string) (string, error) {
+func (s setlistFMService) getArtistMBID(ctx context.Context, artist string) (string, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("%s/search/artists", s.baseURL), nil)
 	if err != nil {
 		return "", fmt.Errorf("creating request: %w", err)
@@ -152,7 +83,7 @@ func (s SetlistFMService) getArtistMBID(ctx context.Context, artist string) (str
 }
 
 // getArtistSetlists retrieves the setlists for an artist's MBID
-func (s SetlistFMService) getArtistSetlists(ctx context.Context, mbid string) ([]domain.Setlist, error) {
+func (s setlistFMService) getArtistSetlists(ctx context.Context, mbid string) ([]domain.Setlist, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("%s/artist/%s/setlists", s.baseURL, mbid), nil)
 	if err != nil {
 		return nil, fmt.Errorf("creating request: %w", err)
