f03e96bb586b192c2cf3abedf94f543b9233c682
Michael Heyman <michaelheyman@users.noreply.github.com>
2025-12-20 14:12:16 -0800

    Update command description

--- cmd/playlist.go
+++ cmd/playlist.go
@@ -42,12 +42,12 @@ func NewCreatePlaylistCmd(
 	cmd := &cobra.Command{
 		Use:   "create-playlist",
 		Short: "Create a Spotify playlist from an artist's setlist",
-		Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+		Long: `Create a Spotify playlist from a live setlist.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Fetches recent setlists for an artist and creates a Spotify playlist with the songs
+performed at those shows.
+
+Requires Spotify and setlist.fm API credentials.`,
 		PreRunE: createCmd.PreRunE,
 		RunE:    createCmd.RunE,
 	}
